2b492544153cc8de32e11b549cccad87bdc93b6d
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-06-01 14:21:21 -0300

    chore: firstday NLW

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('Listagem de usuários');
+
+  response.json([
+    'Diego',
+    'Matheus',
+    'Erbetta',
+    'Karneiro'
+  ]);
+});
+
+app.listen(3333);
