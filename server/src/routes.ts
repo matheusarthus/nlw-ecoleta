824da2245fc26ca213061f2eea79357696c38162
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-06-02 17:11:02 -0300

    chore: second day NLW

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 
+import pointsController from './controllers/PointsController';
+import itemsController from './controllers/ItemsController';
+
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-  return response.json({ message: 'HEllo world' });
-});
+// index, show, create, update, delete
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
